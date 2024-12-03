4320b15bb4abf9db2e4d174be3347e57866734c8
skiminnetonka <94250871+skiminnetonka@users.noreply.github.com>
2024-12-03 11:11:20 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_MOcBzREaj', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '11caq7at2ckba38mcube288r79', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        client_secret: '1qdt9pckipaii6pr7h2oh6kr9j37en2272s528hure5fjs742svk,
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
